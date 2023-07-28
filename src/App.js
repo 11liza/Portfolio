f0ceed404c9d0d506d92c711870177e015832c1f
liza <113092354+11liza@users.noreply.github.com>
2023-07-28 15:06:56 +0200

    build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Header from './components/Header';
 import Skills from './components/Skills';
-import Project from './components/Project';
 import Projects from './components/Projects';
 import './App.css';
 
@@ -19,7 +18,7 @@ const App = () => {
       technologies: ['React', 'HTML5', 'CSS3'],
       demoLink: 'https://game-portfolio-bay.vercel.app/',
       githubLink: 'https://github.com/11liza/Game-Portfolio',
-      imageSrc: '..src/img/game-thumbnail.png', // Replace with the actual image path
+      imageSrc: './src/img/game-thumbnail.png', // Replace with the actual image path
     },
   ];
   
